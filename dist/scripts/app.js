58a05c5da43f38151a909e4f81ec3171fdf294fc
Hannah Jordan <hjorda0126@aol.com>
2015-06-24 02:15:25 -0400

    removing ignores

---
+++ dist/scripts/app.js
@@ -0,0 +1,16 @@
+require.register("main", function(exports, require, module){
+  "use strict";
+
+(function () {
+  "use strict";
+
+  $(document).ready(function () {
+    $("#interest").click(function () {
+      $("#form").toggle();
+    });
+  });
+})();
+  
+});
+
+//# sourceMappingURL=app.js.map
